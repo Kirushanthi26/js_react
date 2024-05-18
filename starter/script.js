47eefa0d47dc1930732311945d9b1c57613d1784
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-05-18 11:19:50 +0530

    filter

--- starter/script.js
+++ starter/script.js
@@ -226,3 +226,14 @@ const essentialData = books.map((book) => ({
   reviewsCount: getTotalReviewCount(book),
 }));
 essentialData;
+
+//condition statisfy -> value return or filtered out
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((books) => books.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
