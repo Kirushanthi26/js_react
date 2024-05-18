9b8d0d35232ec03c6a615503768168a37decd939
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-05-18 10:52:30 +0530

    map

--- starter/script.js
+++ starter/script.js
@@ -210,3 +210,19 @@ const getTotalReviewCount = (book) => {
 };
 
 console.log(getTotalReviewCount(book));
+
+const books = getBooks();
+books;
+
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount(book),
+}));
+essentialData;
